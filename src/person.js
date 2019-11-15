af78a5f87398c3b705b2c0b27e00655ac8d6ba10
Student <example@example.com>
2019-11-15 09:36:50 -0800

    testing updated BASH

---
+++ src/person.js
@@ -0,0 +1,11 @@
+export default class Person {
+  constructor(age, health) {
+    this.age = age,
+    this.lifeExpectancy = 0,
+    this.health = health
+
+  }
+  setExpectancy() {
+    this.lifeExpectancy
+  }
+}
