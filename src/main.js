88213a5b55a7e55c6488167b128604fe75d30d67
Student <example@example.com>
2019-11-15 11:16:08 -0800

    wireframed UI, began work on HTML

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Age from './age.js';
 import Person from './person.js';
 import './styles.css';
 import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 $(document).ready(function() {
