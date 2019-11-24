6b3b8df36929b51aa987019d3455bc6db5697aa0
Lillian Theuma <definatelyruby@gmail.com>
2019-11-24 14:50:14 -0800

    wrote planet.js object code, and planetlist.js object code. Tests successfully. No errors.

---
+++ src/planet.js
@@ -0,0 +1,7 @@
+export default class Planet {
+  constructor(name, adjective, period) {
+    this.name = name,
+    this.adjective = adjective,
+    this.period = period
+  }
+}
---
+++ src/planetlist.js
@@ -0,0 +1,8 @@
+export default class PlanetList {
+  constructor() {
+    this.planets = []
+  }
+  addPlanet(planet){
+    this.planets.push(planet);
+  }
+}
